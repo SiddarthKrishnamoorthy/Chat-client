529701819c313f99e98acb178b5d13210d002686
K Siddarth <siddarth@iitk.ac.in>
2017-11-02 22:00:00 +0530

    Finished client boilerplate

--- client.py
+++ client.py
@@ -20,9 +20,24 @@ password = input("Enter password: ")
 # TODO: Send username and password, if correct log in, else quit
 
 while True:
-    # socket_list = [sys.stdin, s]
-    sys.stdout.write('[You] ')
-    msg = sys.stdin.readline()
-    sys.stdout.flush()
+    socket_list = [sys.stdin, s]
+
+    # Getting readable socket lists
+    ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [])
+    for sock in ready_to_read:
+        if sock == s:
+            # data coming from server
+            data = sock.recv(1000)
+            if data:
+                sys.stdout.write(data + '\n')
+                sys.stdout.write('[You] ')
+                sys.stdout.flush()
+            else:
+                print('Disconnected from server')
+                sys.exit(1)
+        else:
+            sys.stdout.write('[You] ')
+            msg = sys.stdin.readline()
+            sys.stdout.flush()
 
 
